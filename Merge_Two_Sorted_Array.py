1c792e43e8ebfac3ce902bf70aa190201e48cb03
Lalit_mohan <lalitg6376@gmail.com>
2026-01-18 08:54:38 +0530

    Add files via upload

---
+++ Merge_Two_Sorted_Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+       
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+
+nums1 = [1, 3, 5, 0, 0, 0]
+m = 3
+nums2 = [2, 4, 6]
+n = 3
+
+obj = Solution()
+obj.merge(nums1, m, nums2, n)
+
+print(nums1)
