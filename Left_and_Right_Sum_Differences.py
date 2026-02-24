7486bb667ed3aeaf62599eb3fec5c53f4399b2c0
Lalit_mohan <lalitg6376@gmail.com>
2026-02-24 20:49:56 +0530

    Update Left_and_Right_Sum_Differences.py

--- Left_and_Right_Sum_Differences.py
+++ Left_and_Right_Sum_Differences.py
@@ -1,5 +1,4 @@
 # Define two arrays leftSum and rightSum where:
-
 # leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
 # rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
 # Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
